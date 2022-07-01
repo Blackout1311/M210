ddf0b530032a04b4f2e7d22745eb0b494ec36ff9
Blackout1311 <mikka.widmer@gmx.ch>
2022-07-01 10:12:05 +0200

    no fin rickroll

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function easteregg(){
-    window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    window.location.href="https://i1.sndcdn.com/artworks-bWSxzfBdaENYAeO7-Sh7dZQ-t500x500.jpg"
 }
